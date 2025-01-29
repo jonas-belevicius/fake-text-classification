b56bafda679097cda6163ea1b9dfcda1c686dca3
Jonas Belevicius <jonas.belevicius@yahoo.com>
2025-01-29 21:08:20 +0200

    Adding notebook

---
+++ utils/__init__.py
@@ -0,0 +1,5 @@
+import os
+
+PROJECT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+DATA_DIR = os.path.join(PROJECT_DIR, "data")
+UTILS_DIR = os.path.join(PROJECT_DIR, "utils")
